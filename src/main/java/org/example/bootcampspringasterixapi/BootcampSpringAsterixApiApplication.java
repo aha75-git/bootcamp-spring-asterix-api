ff9974a3d946175940d6bf4ef05e487897dd5cfe
lazio <ahaffner75@gmail.com>
2025-09-15 11:05:21 +0200

    Initial commit

---
+++ src/main/java/org/example/bootcampspringasterixapi/BootcampSpringAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootcampspringasterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSpringAsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampSpringAsterixApiApplication.class, args);
+    }
+
+}
